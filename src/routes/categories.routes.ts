ccaef115d7ab4ad4bc3a3f0ac470ca09170e83f4
Gabriel <gabrielmvelezmoro592@gmail.com>
2021-12-31 16:51:16 -0300

    feat: aplica formatação com eslint e cria o services de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,52 +1,21 @@
-import {Router} from 'express';
-<<<<<<< HEAD
+import { Router } from "express";
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
-
-const categoriesRepository = new CategoriesRepository()
-const categoriesRoutes = Router()
-
-=======
-import {v4 as uuidV4} from 'uuid'
-import { Category } from '../model/Category';
-
-const categoriesRoutes = Router()
-
-const categories = []
->>>>>>> 3ef1d81e55a9d7b86ae8eeb862b9ac2b5cbf1ab9
+const categoriesRepository = new CategoriesRepository();
+const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (request, response) => {
-    const {name, description} = request.body
-
-<<<<<<< HEAD
-    const categoryAlreadyExists = categoriesRepository.findByName(name)
-
-    if (categoryAlreadyExists){
-        return response.status(400).json({error: "Category already exists"})
-    }
-
-    categoriesRepository.create({name,description})
-    return response.status(201).send()
-})
+  const { name, description } = request.body;
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
+  return response.status(201).send();
+});
 
 categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRepository.list()
-    return response.json(all)
-})
+  const all = categoriesRepository.list();
+  return response.json(all);
+});
 
-=======
-    
-    const category: Category = {
-        name,
-        description,
-        created_at: new Date()
-    }
-    categories.push(category)
-    console.log(categories)
-
-    return response.status(201).send()
-})
-
->>>>>>> 3ef1d81e55a9d7b86ae8eeb862b9ac2b5cbf1ab9
-export {categoriesRoutes}
+export { categoriesRoutes };
