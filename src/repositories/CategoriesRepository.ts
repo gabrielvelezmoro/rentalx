13da4fb410486e21382873fdbb0b35e4804a5583
Gabriel <gabrielmvelezmoro592@gmail.com>
2022-01-01 17:35:11 -0300

    feat: liskov principle

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,5 +1,8 @@
 import { Category } from "../model/Category";
-import { ICategoriesRepository } from "./ICategoriesRepository";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[] = [];
