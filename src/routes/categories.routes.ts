78a67544954cb05fe2e2bdb02254ea9652fdbf66
Gabriel <gabrielmvelezmoro592@gmail.com>
2022-01-02 11:58:30 -0300

    refactor: useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,12 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 
 const categoriesRepository = new CategoriesRepository();
 const categoriesRoutes = Router();
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-  return response.status(201).send();
-});
+categoriesRoutes.post("/", (request, response) => {});
 
 categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
