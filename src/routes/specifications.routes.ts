6706282971e429ff8c2462e591ed26b595664d54
Gabriel <gabrielmvelezmoro592@gmail.com>
2022-03-27 00:38:35 -0300

    refactor: exporta estrutura de criacao de especificacao ja construida via arquivo de index

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,19 +1,12 @@
 import { Router } from "express";
 
 import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
 
 const specificationsRepository = new SpecificationsRepository();
 const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
-});
+specificationsRoutes.post("/", (request, response) => {});
 
 specificationsRoutes.get("/", (request, response) => {
   const all = specificationsRepository.list();
