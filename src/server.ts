4102a4ffce2ca5b101a409a6b922c5220179c698
gabriel <gabrielmvelezmoro592@gmail.com>
2022-06-18 22:00:14 -0300

    chore: habilita decorators

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
-
+import "reflect-metadata";
 import { router } from "./routes";
 
 const app = express();
