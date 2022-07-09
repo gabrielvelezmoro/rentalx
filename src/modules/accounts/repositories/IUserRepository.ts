f3b61ffb607ac5797490ec6636809864f5cd7bcd
gabriel <gabrieuvelezmoro@gmail.com>
2022-07-09 12:08:19 -0300

    feat: verify if user already exists by email

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -1,5 +1,7 @@
+import { User } from "../entities/User";
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
